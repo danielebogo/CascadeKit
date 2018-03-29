69f0cda94250e51810b3694cbe25ac495da37ce7
Daniele Bogo <Daniele.Bogo@net-a-porter.com>
2018-03-29 16:33:56 +0100

    Add more alphabets

--- UnicodeScalarParser/Alphabets/Alphabet.swift
+++ UnicodeScalarParser/Alphabets/Alphabet.swift
@@ -9,8 +9,10 @@ import Foundation
 enum Alphabet {
     case arabic
     case greek
+    case greekExtended
     case latin
     case latinSupplementary
+    case myanmar
     case russian
     case russianSupplementary
 
@@ -20,8 +22,10 @@ enum Alphabet {
         switch self {
         case .arabic: return 0x600...0x6FF
         case .greek: return 0x370...0x3FF
+        case .greekExtended: return 0x1F00...0x1FFF
         case .latin: return 0x20...0x7F
         case .latinSupplementary: return 0xA0...0xFF
+        case .myanmar: return 0x1000...0x109F
         case .russian: return 0x400...0x4FF
         case .russianSupplementary: return 0x500...0x52F
         }
