786844fbd2759b92c166331121361309942360db
Daniele Bogo <me@bogodaniele.com>
2018-04-25 14:14:56 +0100

    Make public init to avoid Memberwise Initializer default behaviour

--- Cascade/Models/Attribute.swift
+++ Cascade/Models/Attribute.swift
@@ -6,4 +6,11 @@ public struct Attribute {
     public var key: NSAttributedStringKey
     public var value: Any
     public var range: CountableClosedRange<Int>
+    
+    
+    public init(key: NSAttributedStringKey, value: Any, range: CountableClosedRange<Int>) {
+        self.key = key
+        self.value = value
+        self.range = range
+    }
 }
