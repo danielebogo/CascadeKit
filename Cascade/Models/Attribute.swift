b0709c61fb1e1a32f4f4dd5ee0611aee0913692e
Daniele Bogo <me@bogodaniele.com>
2018-04-25 13:56:50 +0100

    - Make properties public

--- Cascade/Models/Attribute.swift
+++ Cascade/Models/Attribute.swift
@@ -3,7 +3,7 @@ import Foundation
 
 /// Attribute struct to decorate an attribute string
 public struct Attribute {
-    var key: NSAttributedStringKey
-    var value: Any
-    var range: CountableClosedRange<Int>
+    public var key: NSAttributedStringKey
+    public var value: Any
+    public var range: CountableClosedRange<Int>
 }
